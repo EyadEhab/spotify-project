ae9a99df39c419a2120b1f3687d57fcd9f4f358d
YassinHany1 <23-101235@students.eui.edu.eg>
2024-12-14 16:27:25 +0200

    bases functions class: song

--- 3l_baraka/src/song.h
+++ 3l_baraka/src/song.h
@@ -1,10 +1,42 @@
 #ifndef SONG_H
 #define SONG_H
 
-class song
-{
+#include <QString>
+#include <QDebug>
+#include <QDir>
+#include <QFileInfoList>
+#include <QFileInfo>
+#include <QStringList>
+
+class Song {
+private:
+    QString title;   // Song title
+    QString artist;  // Artist name
+    float duration;    // Duration in seconds
+    int plays;       // Play count
+
 public:
-    song();
+    // Constructor
+    Song(const QString& title, const QString& artist, int duration);
+
+
+
+    // Getters
+    QString getTitle() const;
+    QString getArtist() const;
+    float getDuration() const;
+    int getPlays() const;
+
+    // Setters
+    void setTitle(const QString& newTitle);
+    void setArtist(const QString& newArtist);
+    void setDuration(float newDuration);
+    void searchAndPlay(const QString& songName); // Searches for a song by title and plays it
+    void searchAndPlay(const QString& songName, const QString& directory);
+
+    // Functionalities
+    void playSong();
+    void displaySongInfo() const;
 };
 
 #endif // SONG_H
