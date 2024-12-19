1fc5c893461e32afeb003a6cdf76a0dccc0026ee
EyadEhab <23-101054@students.eui.edu.eg>
2024-12-19 16:40:29 +0200

    added hello world in main.cpp

---
+++ 3l_baraka/build/CMakeFiles/3l_baraka.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/eyad/a5ca1888-ccc2-42dc-9e91-871ec5dce3b3/eyad/All/cis year 2/first term/data struc/spotify-project/3l_baraka/src/main.cpp" "CMakeFiles/3l_baraka.dir/src/main.cpp.o" "gcc" "CMakeFiles/3l_baraka.dir/src/main.cpp.o.d"
+  "/media/eyad/a5ca1888-ccc2-42dc-9e91-871ec5dce3b3/eyad/All/cis year 2/first term/data struc/spotify-project/3l_baraka/src/node.cpp" "CMakeFiles/3l_baraka.dir/src/node.cpp.o" "gcc" "CMakeFiles/3l_baraka.dir/src/node.cpp.o.d"
+  "/media/eyad/a5ca1888-ccc2-42dc-9e91-871ec5dce3b3/eyad/All/cis year 2/first term/data struc/spotify-project/3l_baraka/src/playlist.cpp" "CMakeFiles/3l_baraka.dir/src/playlist.cpp.o" "gcc" "CMakeFiles/3l_baraka.dir/src/playlist.cpp.o.d"
+  "/media/eyad/a5ca1888-ccc2-42dc-9e91-871ec5dce3b3/eyad/All/cis year 2/first term/data struc/spotify-project/3l_baraka/src/song.cpp" "CMakeFiles/3l_baraka.dir/src/song.cpp.o" "gcc" "CMakeFiles/3l_baraka.dir/src/song.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 3l_baraka/build/CMakeFiles/3l_baraka.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "3l_baraka"
+  "3l_baraka.pdb"
+  "CMakeFiles/3l_baraka.dir/src/main.cpp.o"
+  "CMakeFiles/3l_baraka.dir/src/main.cpp.o.d"
+  "CMakeFiles/3l_baraka.dir/src/node.cpp.o"
+  "CMakeFiles/3l_baraka.dir/src/node.cpp.o.d"
+  "CMakeFiles/3l_baraka.dir/src/playlist.cpp.o"
+  "CMakeFiles/3l_baraka.dir/src/playlist.cpp.o.d"
+  "CMakeFiles/3l_baraka.dir/src/song.cpp.o"
+  "CMakeFiles/3l_baraka.dir/src/song.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3l_baraka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
